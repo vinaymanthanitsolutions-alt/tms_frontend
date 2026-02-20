015e0e882eb50856e5bb3bcd67ad38f34ccb968f
sharkiiee <sarthak231203@gmail.com>
2026-02-20 13:54:00 +0530

    completed adminRiskOverview design

--- src/data.js
+++ src/data.js
@@ -6,9 +6,9 @@
 //     role : "teamlead"
 // }
 
-export const DEMODATA = {
-  role: "projectmanager",
-};
+// export const DEMODATA = {
+//   role: "projectmanager",
+// };
 
 // export const DEMODATA = {
 //  role : "projectmanager"
